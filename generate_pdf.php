206b6b688d016637e21a2ec158224ac18103a57c
sturyczbd02 <78055378+sturyczbd02@users.noreply.github.com>
2023-04-26 16:47:07 -0500

    updated pdf stuff, still not working yet

--- generate_pdf.php
+++ generate_pdf.php
@@ -1,33 +1,27 @@
 <?php
 if (isset($_POST['submit'])) {
+    // Include the TCPDF single file version
+    require('tcpdf/tcpdf.php');
+
     // Get the HTML content of the form
     $html = file_get_contents('from-scratch.html');
 
-    // Create a new PDF document
-    $pdf = new \PDFlib();
+    // Create a new TCPDF object
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
     // Set document information
-    $pdf->set_info('Creator', 'Your Name');
-    $pdf->set_info('Author', 'Your Name');
-    $pdf->set_info('Title', 'Form Submission');
-    $pdf->set_info('Subject', 'Form Submission');
+    $pdf->SetCreator(PDF_CREATOR);
+    $pdf->SetAuthor('Your Name');
+    $pdf->SetTitle('Form Submission');
+    $pdf->SetSubject('Form Submission');
 
-    // Open a new page
-    $pdf->begin_page_ext(0, 0, 'width=a4.width height=a4.height');
-
-    // Define a font
-    $font = $pdf->load_font('Helvetica', 'winansi', '');
+    // Add a page
+    $pdf->AddPage();
 
     // Write the HTML content to the PDF
-    $pdf->setfont($font, 12);
-    $pdf->fit_textflow($html, 50, 50, 550, 700, '');
-
-    // Close the page
-    $pdf->end_page_ext('');
+    $pdf->writeHTML($html);
 
     // Output the generated PDF to the browser
-    header('Content-Type: application/pdf');
-    header('Content-Disposition: inline; filename="character-sheet.pdf"');
-    echo $pdf->get_buffer();
+    $pdf->Output('character-sheet.pdf', 'I');
 }
 ?>
