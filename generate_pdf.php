b33b3a9862df16aba7d54d9c4a0fd18e5e76941b
MumbowerCJ23 <MumbowerCJ23@uww.edu>
2023-05-02 18:31:51 -0500

    fixed

--- generate_pdf.php
+++ generate_pdf.php
@@ -149,20 +149,6 @@ if (isset($_POST['submit'])) {
     $pdf->Cell(0, 10, $_POST['spells'], 0, 1, 'L');
 
     $html = <<<EOF
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-
-    <link rel="stylesheet" href="styles.css">
-
 
 <div id="step-1">
     <h2>Step 1: Character Info, Race, and Class</h2>
