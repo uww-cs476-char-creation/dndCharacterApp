c57bf579fbd80ae009570181b5508900fc75743d
MumbowerCJ23 <MumbowerCJ23@uww.edu>
2023-05-02 19:17:32 -0500

    eof test

--- generate_pdf.php
+++ generate_pdf.php
@@ -224,7 +224,13 @@ if (isset($_POST['submit'])) {
         <td width="100" align="center"><b>Class</b></td>
      </tr>
      <tr>
-        <td width="100" align="center">.$_POST['pname'].</td>
+        <td width="100" align="center">
+        EOF;
+        $pdf->writeHTML($html, true, false, true, false, '');
+        echo $_POST['pname'];
+
+        $html = <<<EOF
+        </td>
         <td width="100" align="center">Character Name</td>
         <td width="100" align="center">Alignment</td>
         <td width="100" align="center">Background</td>
