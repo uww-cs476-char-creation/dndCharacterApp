506bedf51bd210da56b6121e9f02a9ae4535f1e7
sturyczbd02 <78055378+sturyczbd02@users.noreply.github.com>
2023-05-03 11:37:45 -0500

    working on import

--- import.php
+++ import.php
@@ -2,64 +2,45 @@
 
 require_once('/home/projects/cs476/dndCharacterApp/tcpdf_6_3_2/tcpdf/tcpdf.php');
 
-// Check if the form has been submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Check if the PDF file was uploaded
+if (isset($_FILES['pdfFile'])) {
+  $file = $_FILES['pdfFile']['tmp_name'];
 
-    // Check if a PDF file was uploaded
-    if (isset($_FILES['pdfFile']) && $_FILES['pdfFile']['error'] === UPLOAD_ERR_OK) {
+  // Create a new TCPDF object
+  $pdf = new TCPDF();
+  $pdf->setSourceFile($file);
 
-        // Create a new TCPDF instance
-        $pdf = new TCPDF();
+  // Extract the text from the first page of the PDF
+  $text = $pdf->getTextByPage(1);
 
-        // Get the contents of the uploaded PDF file
-        $pdfData = file_get_contents($_FILES['pdfFile']['tmp_name']);
+  // Parse the text to extract the relevant information
+  preg_match('/Player Name: (.*)/', $text, $matches);
+  $player_name = isset($matches[1]) ? $matches[1] : '';
+  preg_match('/Character Name: (.*)/', $text, $matches);
+  $character_name = isset($matches[1]) ? $matches[1] : '';
+  preg_match('/Alignment: (.*)/', $text, $matches);
+  $alignment = isset($matches[1]) ? $matches[1] : '';
+  preg_match('/Background: (.*)/', $text, $matches);
+  $background = isset($matches[1]) ? $matches[1] : '';
+  preg_match('/Race: (.*)/', $text, $matches);
+  $race = isset($matches[1]) ? $matches[1] : '';
+  preg_match('/Character Level: (.*)/', $text, $matches);
+  $level = isset($matches[1]) ? $matches[1] : '';
+  preg_match('/Class: (.*)/', $text, $matches);
+  $class = isset($matches[1]) ? $matches[1] : '';
 
-        // Extract the text from the PDF file
-        $text = $pdf->getTextFromPdfData($pdfData);
+  // Load the HTML form
+  $html = file_get_contents('import.html');
 
-        // Parse the text to extract the relevant data
-        $playerName = extractValue($text, 'Player Name:');
-        $characterName = extractValue($text, 'Character Name:');
-        $alignment = extractValue($text, 'Alignment:');
-        $background = extractValue($text, 'Background:');
-        $race = extractValue($text, 'Race:');
-        $level = extractValue($text, 'Level:');
-        $class = extractValue($text, 'Class:');
+  // Replace the placeholders in the HTML form with the extracted data
+  $html = str_replace('{player_name}', $player_name, $html);
+  $html = str_replace('{character_name}', $character_name, $html);
+  $html = str_replace('{alignment}', $alignment, $html);
+  $html = str_replace('{background}', $background, $html);
+  $html = str_replace('{race}', $race, $html);
+  $html = str_replace('{level}', $level, $html);
+  $html = str_replace('{class}', $class, $html);
 
-        // Load the import.html file into a string variable
-        $html = file_get_contents('import.html');
-
-        // Replace the placeholders in the HTML string with the extracted data
-        $html = str_replace('{player_name}', $playerName, $html);
-        $html = str_replace('{character_name}', $characterName, $html);
-        $html = str_replace('{alignment}', $alignment, $html);
-        $html = str_replace('{background}', $background, $html);
-        $html = str_replace('{race}', $race, $html);
-        $html = str_replace('{level}', $level, $html);
-        $html = str_replace('{class}', $class, $html);
-
-        // Output the modified HTML string
-        echo $html;
-
-    } else {
-        // Handle errors uploading the file
-        echo "Error uploading PDF file.";
-    }
+  // Output the HTML form
+  echo $html;
 }
-
-/**
- * Extracts a value from a text string by searching for a keyword and returning the text after it.
- */
-function extractValue($text, $keyword) {
-    $startPos = strpos($text, $keyword);
-    if ($startPos !== false) {
-        $startPos += strlen($keyword);
-        $endPos = strpos($text, "\n", $startPos);
-        if ($endPos !== false) {
-            return trim(substr($text, $startPos, $endPos - $startPos));
-        }
-    }
-    return "";
-}
-
-?>
