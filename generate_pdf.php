08d7d40adae302170427c4c94888d2154e54eb62
sturyczbd02 <78055378+sturyczbd02@users.noreply.github.com>
2023-05-04 20:44:19 -0500

    maybe v2

--- generate_pdf.php
+++ generate_pdf.php
@@ -24,7 +24,7 @@ if (isset($_POST['submit'])) {
     $pdf->SetFont('helvetica', '', 14);
     $pdf->Cell(50, 10, 'Player Name:', 0, 0, 'R');
     $pdf->Cell(100, 10, $_POST['pname'], 0, 0, 'L');
-    $pdf->Cell(50, 10, 'Character Name:', 0, 0, 'R');
+    $pdf->Cell(50, 10, 'Character Name:', 0, 0, 'L');
     $pdf->Cell(100, 10, $_POST['cname'], 0, 1, 'L');
     $pdf->Cell(50, 10, 'Race:', 0, 0, 'R');
     $pdf->Cell(0, 10, $_POST['race'], 0, 1, 'L');
